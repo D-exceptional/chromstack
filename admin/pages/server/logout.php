4307dcb7cbbb979416542b792bf03ad267fe5d74
Okeke Chukwuebuka <chukwuebukaokeke09@gmail.com>
2025-09-10 01:35:25 +0100

    Minor tracking rule changes

--- admin/pages/server/logout.php
+++
@@ -1,5 +0,0 @@
-<?php 
-    session_start();
-    unset($_SESSION['adminID']);
-    header('Location: /admin/index');
-?>
--- affiliate/server/logout.php
+++
@@ -1,5 +0,0 @@
-<?php 
-    session_start();
-    unset($_SESSION['affiliateID']);
-    header('Location: /login');
-?>
